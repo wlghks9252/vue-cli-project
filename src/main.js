bcb1497e36c56e21ace9985d5c38e9231298c273
jhpark <jhpark@gaia3d.com>
2023-02-25 13:10:46 +0900

    4~7 강 EventBus 전까지

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import StatusComponents from './Status.vue'
-
-/* Vue.component('AppStatus', StatusComponents) */
 
 loadFonts()
 
@@ -14,6 +11,4 @@ createApp(App)
   .use(router)
   .use(store)
   .use(vuetify)
-  .component('Appstatus',StatusComponents)
   .mount('#app')
-  
